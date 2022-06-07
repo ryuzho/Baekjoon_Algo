7e1883f64bb1e095515b5207fc48603cdaf36031
ryuzho <fbwlgh07@ajou.ac.kr>
2022-06-08 00:43:07 +0900

    17298

--- 17298.py
+++ 17298.py
@@ -2,8 +2,32 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-A = list(map(int, input().split()))
+A = int(input())
+arr = list(map(int, input().split()))
+nums = []
+stack = []
+sol = [0]*A
+for i, v in enumerate(arr):
+    nums.append((v,i))
 
-for i, v in enumerate(A):
-    for j in
+for num in nums:
+
+    if not stack:
+        stack.append(num)
+
+    else:
+        while True:
+            if stack and num[0] > stack[-1][0]:
+                v, i = stack.pop()
+                sol[i] = num[0]
+            else:
+                stack.append(num)
+                break
+
+for i in sol:
+    if i == 0:
+        print("-1", end = ' ')
+    else:
+        print(i, end = ' ')
+
+
